b90e2d7a4ca144f9f3b23d293c671213260c5004
raghavsharma <raghavs@sigmoidanalytics.com>
2021-08-24 18:47:00 +0530

    Added 'run_query' method, refactored code for readability

---
+++ ApiConnectorService.py
@@ -0,0 +1,235 @@
+import requests
+from requests.exceptions import HTTPError
+
+"""
+This class fires the firebolt apis to generate access-token and further engine url. The two attributes are then
+shared with the dialect class to retrieve the metadata from Firebolt DB.
+"""
+
+
+class ApiConnectorService:
+
+    def __init__(self, user_email, password):
+        self._user_email = user_email  # provided by firebolt
+        self._password = password  # provided by firebolt
+
+    # retrieve authentication token
+    """
+    This method uses the user email and the password to fire the API to generate access-token.
+    :input token url, request type of API and authentication header
+    :returns access-token
+    """
+
+    def get_access_token(self, token_url, request_type, header):
+        json_response = ""  # base case
+        try:
+
+            """
+               General format of request:
+
+               curl --request POST 'https://api.app.firebolt.io/auth/v1/login' \
+               --header 'Content-Type: application/json;charset=UTF-8' \
+               --data-binary
+               '{"username":"YOUR_USER_EMAIL","password":"YOUR_PASSWORD"}'
+               """
+
+            token_response = requests.get(
+                "curl --request {0} \'{1}\' \\\n--header \'{2}\' \\\n--data-binary\n\'{\"username\":"
+                "\"{3}\",\"password\":\"{4}\"}\'".format(
+                    request_type, token_url, header, self._user_email, self._password))
+            token_response.raise_for_status()
+
+            """
+            General format of response:
+
+            {  
+              "access_token": "YOUR_ACCESS_TOKEN_VALUE",  
+              "expires_in": 86400,  
+              "refresh_token": "YOUR_REFRESH_TOKEN_VALUE",  
+              "scope": "offline_access",  
+              "token_type": "Bearer"  
+            }
+            """
+
+            json_response = token_response.json()
+
+            # print("Entire JSON response")
+            # print(jsonResponse)
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+
+        return json_response
+
+    # refresh access token
+    """
+    In case the token expires or the API throws a 401 HTTP error, then this method generates a fresh token
+    :input refresh api url, request type, authentication header and 
+    the refresh token generated alongside the previous expired token
+    :returns new access-token
+    """
+
+    def get_access_token_via_refresh(self, refresh_url, request_type, header, refresh_token):
+        refresh_access_token = 0
+        try:
+            """
+                Request:
+                curl --request POST 'https://api.app.firebolt.io/auth/v1/refresh' \  
+                --header 'Content-Type: application/json;charset=UTF-8' \  
+                --data-binary '{"refresh_token":"YOUR_REFRESH_TOKEN_VALUE"}'
+                """
+            token_response = requests.get(
+                "curl --request {0} \'{1}\' \\\n--header \'{2}\' \\\n--data-binary\n\'{\"refresh_token\":"
+                "\"{3}\"}\'".format(
+                    request_type, refresh_url, header, refresh_token))
+            token_response.raise_for_status()
+
+            """
+            Response:
+            {
+                "access_token": "YOUR_REFRESHED_ACCESS_TOKEN_VALUE",
+                "scope": "offline_access",
+                "expires_in": 86400,
+                "token_type": "Bearer"
+            }
+            """
+
+            jsonResponse = token_response.json()
+            refresh_access_token = jsonResponse["access_token"]
+
+            # print("Entire JSON response")
+            # print(jsonResponse)
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+
+        return refresh_access_token
+
+    # get engine url by db name
+    """
+    This method generates engine url using db name and access-token
+    :input api url, request type, authentication header and access-token
+    :returns engine url
+    """
+
+    def get_engine_url_by_db(self, engine_db_url, request_type, header, token):
+        engine_url = ""
+        try:
+            """
+                Request:
+
+                curl --request GET 'https://api.app.firebolt.io/core/v1/account/engines:getURLByDatabaseName?database_name=YOUR_DATABASE_NAME' \  
+                --header 'Authorization: Bearer YOUR_ACCESS_TOKEN_VALUE'
+                """
+            query_engine_response = requests.get(
+                "curl --request {0} \'{1}\' \\\n--header \'{2}{3}\'".format(
+                    request_type, engine_db_url, header, token))
+            query_engine_response.raise_for_status()
+
+            """
+            Response:
+            {"engine_url": "YOUR_DATABASES_DEFAULT_ENGINE_URL"}
+            """
+            jsonResponse = query_engine_response.json()
+            engine_url = jsonResponse["engine_url"]
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+
+        return engine_url
+
+    # get engine url by engine name
+    """
+    This method generates engine url using engine name and access-token
+    :input api url, request type, authentication header and access-token
+    :returns engine url
+    """
+
+    def get_engine_url_by_name(self, engine_url, request_type, header, token):
+        engine_url = ""
+        try:
+            """ Request:
+                curl --request GET 'https://api.app.firebolt.io/core/v1/account/engines?filter.name_contains=YOUR_ENGINE_NAME' \  
+                --header 'Authorization: Bearer YOUR_ACCESS_TOKEN_VALUE'
+                """
+            query_engine_response = requests.get(
+                "curl --request {0} \'{1}\' \\\n--header \'{2}{3}\'".format(
+                    request_type, engine_url, header, token))
+            query_engine_response.raise_for_status()
+            # access JSOn content
+
+            """
+            Response:
+            {
+              "page": {
+                ...
+              },
+              "edges": [
+                {
+                  ...
+                    "endpoint": "YOUR_ENGINE_URL",
+                  ...
+                  }
+                }
+              ]
+            }
+            """
+
+            jsonResponse = query_engine_response.json()
+            edges = jsonResponse['edges']
+            engine_url = edges["endpoint"]
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+
+        return engine_url
+
+    # run queries
+    """
+    This method is used to submit a query to run to a running engine. 
+    You can specify multiple queries separated by a semicolon (;)..
+    :input token url, request type of API and authentication header
+    :returns access-token
+    """
+
+    def run_query(self, query_url, request_type, header, token):
+        json_response = ""  # base case
+        try:
+
+            """
+            Request:
+            echo "SELECT_QUERY" | curl
+            --request POST 'https://YOUR_ENGINE_ENDPOINT/?database=YOUR_DATABASE_NAME' \
+            --header 'Authorization: Bearer YOUR_ACCESS_TOKEN_VALUE' \
+            --data-binary @-
+            """
+
+            query_response = requests.get(
+                "echo \"SELECT_QUERY\" | curl\n--request {0} \'{1}\' \\\n--header \'{2}{3}\' \\\n--data-binary @-".format(
+                    request_type, query_url, header, token))
+            query_response.raise_for_status()
+
+            """
+            Response:
+            yet to receive firebolt access to test on local
+            """
+
+            json_response = query_response.json()
+
+            # print("Entire JSON response")
+            # print(jsonResponse)
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+
+        return json_response
--- Controller.py
+++ Controller.py
@@ -1,15 +1,24 @@
-import Sqlalchemy
+import ApiConnectorService
 
+"""
+Utility class to interlink modules of Sqlalchemy Adapter
+To be merged with AlchemyConnector
+"""
 
 class Controller:
 
-    def __init__(self, request):
-        self.query = request
-        self.token = token
-        self.url = url
+    def __init__(self, user_email, password):
+        self._user_email = user_email
+        self._password = password
+        self._token = 0;
+        self._engine_url = ""
 
+    """
+    Method to call ApiConnectorService in order to fire respective Firebolt APIs 
+    and retrieve data required for the dialect
+    """
     def get_connection_details(self):
-        api_service = Sqlalchemy()  # provide user email and password here
+        api_service = ApiConnectorService(self._user_email, self._password)  # provide user email and password here
         token_url = "https://api.app.firebolt.io/auth/v1/login"
         token_request_type = "POST"
         token_header = "Content-Type: application/json;charset=UTF-8"
@@ -18,11 +27,11 @@ class Controller:
         token_json = api_service.get_access_token(token_url, token_request_type, token_header)
         access_token = token_json["access_token"]
         refresh_token = token_json["refresh_token"]
-        if access_token == 0:
-            self.token = api_service.get_access_token_via_refresh(refresh_url, token_request_type, token_header,
+        if access_token == 0:    # flag check for token validity
+            self._token = api_service.get_access_token_via_refresh(refresh_url, token_request_type, token_header,
                                                                   refresh_token)
         else:
-            self.token = access_token
+            self._token = access_token
 
         query_engine_url = 'https://api.app.firebolt.io/core/v1/account/' \
                            'engines:getURLByDatabaseName?database_name=YOUR_DATABASE_NAME'
@@ -33,10 +42,18 @@ class Controller:
         # query_engine_url_name = "https://api.app.firebolt.io/core/v1/
         # account/engines?filter.name_contains=YOUR_ENGINE_NAME"
 
-        self.url = api_service.get_engine_url_by_db(query_engine_url, engine_request_type, engine_header, self.token)
+        self._engine_url = api_service.get_engine_url_by_db(query_engine_url, engine_request_type, engine_header, self._token)
 
-        return self.token, self.url
+        return self._token, self._url
 
+"""
+token_url = "https://api.app.firebolt.io/auth/v1/login"
+token_request_type = "POST"
+token_header = "Content-Type: application/json;charset=UTF-8"
+refresh_url = "https://api.app.firebolt.io/auth/v1/refresh"
 
-controller = Controller(" ")
-dialect = Dialect(controller)
+query_engine_url = 'https://api.app.firebolt.io/core/v1/account/engines:getURLByDatabaseName?database_name=YOUR_DATABASE_NAME'
+engine_request_type = 'GET'
+engine_header = "Authorization: Bearer "
+query_engine_url_name = 'https://api.app.firebolt.io/core/v1/account/engines?filter.name_contains=YOUR_ENGINE_NAME'
+"""
