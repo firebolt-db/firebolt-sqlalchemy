0a522c7b98b4db87de16c5b815ce357895d089d1
raghavsharma <raghavs@sigmoidanalytics.com>
2021-09-08 13:48:01 +0530

    Refactored code for refresh token api

--- firebolt_api_service.py
+++ firebolt_api_service.py
@@ -203,10 +203,16 @@ class FireboltApiService:
             query_response.raise_for_status()
 
         except HTTPError as http_err:
-            payload = {
-                "error": "Run Query API Exception",
-                "errorMessage": http_err.response.text,
-            }
+            if http_err.response.status_code == 401:
+                access_token = FireboltApiService.get_access_token_via_refresh(refresh_token)
+                header = {'Authorization': "Bearer " + access_token}
+                query_response = requests.post(url="https://" + engine_url, params={'database': db_name},
+                                               headers=header, files={"query": (None, query)})
+            else:
+                payload = {
+                    "error": "Run Query API Exception",
+                    "errorMessage": http_err.response.text,
+                }
         except Exception as err:
             payload = {
                 "error": "Run Query API Exception",
