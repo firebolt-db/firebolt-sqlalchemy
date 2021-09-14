7d86d75e99fd555e7d156ea1311a39431e9cf867
Apurva Anand <aapurva@sigmoidanalytics.com>
2021-09-14 19:03:18 +0530

    Changes to fix NoneType return issue

--- src/firebolt_db/firebolt_dialect.py
+++ src/firebolt_db/firebolt_dialect.py
@@ -99,13 +99,10 @@ class FireboltDialect(default.DefaultDialect):
         result = connection.execute(
             "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.DATABASES"
         )
-        # print("hey")
-        # print(type(result))
-        # return [row for row in result]
-        # return result.next().schema_name
-        return [
-            row.schema_name for row in result
-        ]
+        return result
+        # return [
+        #     row.schema_name for row in result
+        # ]
 
     def has_table(self, connection, table_name, schema=None):
         query = """
@@ -127,8 +124,8 @@ class FireboltDialect(default.DefaultDialect):
             )
 
         result = connection.execute(query)
-        # return result
-        return [row.table_name for row in result]
+        return result
+        # return [row.table_name for row in result]
 
     def get_view_names(self, connection, schema=None, **kwargs):
         return []
