43d570f492fe001ea6acb8f9274d21e484d49941
Apurva Anand <aapurva@sigmoidanalytics.com>
2021-09-17 17:29:23 +0530

    New test script

--- temp_test_script.py
+++ temp_test_script.py
@@ -4,7 +4,7 @@
 # Comment out the type of test you want to run
 
 # from firebolt_db.firebolt_connector import connect
-# from firebolt_db.firebolt_dialect import FireboltDialect
+from firebolt_db.firebolt_dialect import FireboltDialect
 
 # connection = connect('localhost',8123,'aapurva@sigmoidanalytics.com', 'Apurva111', 'Sigmoid_Alchemy')
 
@@ -26,4 +26,29 @@
 # tables = dialect.get_table_names(connection,"Sigmoid_Alchemy").fetchall()
 # print("Table names")
 # print(tables)
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.dialects import registry
+registry.register("firebolt", "src.firebolt_db.firebolt_dialect", "FireboltDialect")
+
+engine = create_engine("firebolt://aapurva@sigmoidanalytics.com:Apurva111@host/Sigmoid_Alchemy")
+print(type(engine))
+
+dialect = FireboltDialect()
+
+schemas = dialect.get_schema_names(engine)
+print(schemas)
+# print("Schema Names")
+# print(schemas.fetchone())
+
+schemas = dialect.get_table_names(engine,"Sigmoid_Alchemy")
+print(schemas)
+
+schemas = dialect.get_columns(engine,"lineitem","Sigmoid_Alchemy")
+print(schemas)
+
+connection = engine.connect()
+schemas = dialect.get_schema_names(connection)
+print(schemas)
 
