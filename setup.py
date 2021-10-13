ecb641df667c14765cf92d93753013c98d21b73d
Pop Cosmin <cosmin@pops-mbp.home>
2021-10-13 18:05:35 +0300

    fix: lower case for information_schema.databases

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="firebolt-sqlalchemy",
-    version="0.0.4",
+    version="0.0.5",
     author="Firebolt",
     author_email="pypi@firebolt.io",
     description="Sqlalchemy adapter for Firebolt",
