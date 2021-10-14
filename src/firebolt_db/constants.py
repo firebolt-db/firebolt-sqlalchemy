15b95c6eb65a26171d5936918b7ab6e274a3a191
Apurva Anand <aapurva@sigmoidanalytics.com>
2021-10-14 11:09:36 +0530

    Changed config .ini file to .env file

--- src/firebolt_db/constants.py
+++ src/firebolt_db/constants.py
@@ -1,15 +1,28 @@
-import configparser
 import os
+from dotenv import load_dotenv
 
-config = configparser.ConfigParser()
-config.read(f"{os.getcwd()}/env-config.ini")
+load_dotenv()
+
+
+def get_env_variable(var_name: str) -> str:
+    """Get the environment variable or raise exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "The environment variable {} was missing, abort...".format(
+            var_name
+        )
+        raise EnvironmentError(error_msg)
+
+
+DATABASE_DIALECT = get_env_variable("refresh_url")
 
 base_url = "https://api.app.firebolt.io/"
 
-token_url = f"{base_url}{config['API']['token_url']}"
-refresh_url = f"{base_url}{config['API']['refresh_url']}"
-query_engine_url = f"{base_url}{config['API']['query_engine_url']}:getURLByDatabaseName"
-query_engine_url_by_engine_name = f"{base_url}{config['API']['query_engine_url']}"
-engine_id_url = f"{base_url}{config['API']['engine_id_url']}:getIdbyName"
-engine_start_url = f"{base_url}{config['API']['engine_start_url']}"
+token_url = f"{base_url}{get_env_variable('token_url')}"
+refresh_url = f"{base_url}{get_env_variable('refresh_url')}"
+query_engine_url = f"{base_url}{get_env_variable('query_engine_url')}:getURLByDatabaseName"
+query_engine_url_by_engine_name = f"{base_url}{get_env_variable('query_engine_url')}"
+engine_id_url = f"{base_url}{get_env_variable('engine_id_url')}:getIdbyName"
+engine_start_url = f"{base_url}{get_env_variable('engine_start_url')}"
 token_header = {"Content-Type": "application/json;charset=UTF-8"}
