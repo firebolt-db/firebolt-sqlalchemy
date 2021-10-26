dfe69bd9c7118c93a79f2d9571d8a2cdd43f08cd
Pop Cosmin <cosmin@pops-mbp.home>
2021-10-26 16:31:59 +0300

    add back the setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="firebolt-sqlalchemy",
+    version="0.0.8",
+    author="Firebolt",
+    author_email="pypi@firebolt.io",
+    description="Sqlalchemy adapter for Firebolt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/firebolt-db/firebolt-sqlalchemy",
+    download_url="https://github.com/firebolt-db/firebolt-sqlalchemy/archive/refs/tags/0.0.8.tar.gz",
+    project_urls={
+        "Bug Tracker": "https://github.com/firebolt-db/firebolt-sqlalchemy",
+    },
+    install_requires=[
+        'sqlalchemy>=1.0.0',
+        "requests",
+        "datetime"
+    ],
+    entry_points={
+        "sqlalchemy.dialects": [
+            "firebolt = firebolt_db.firebolt_dialect:FireboltDialect"
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
