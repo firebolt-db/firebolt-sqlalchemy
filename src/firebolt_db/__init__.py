16e48c7d9a72a369dfd3342504743c21104b9b8a
Petro Tiurin <petro.tiurin@firebolt.io>
2021-12-15 19:13:57 +0000

    Removing commented out code

--- src/firebolt_db/__init__.py
+++ src/firebolt_db/__init__.py
@@ -1,14 +1,3 @@
-# from firebolt.db import connect
-# from firebolt.async_db import connect
-# from firebolt.async_db import connect as async_connect
-# import asyncio
-# def connect(*args, **kwargs):
-#     print("Running connect!")
-#     res = get_event_loop().run_until_complete(async_connect(*args, **kwargs))
-#     #asyncio.run(async_connect(*args, **kwargs))
-#     print("Finished connect")
-#     return res
-
 __all__ = [
     "connect",
     "apilevel",
