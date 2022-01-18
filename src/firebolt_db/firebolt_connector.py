3915b66acadd7924f441cb3d5056aed51d08866e
Petro Tiurin <petro.tiurin@firebolt.io>
2022-01-18 16:51:05 +0000

    fix mypy

--- src/firebolt_db/firebolt_connector.py
+++ src/firebolt_db/firebolt_connector.py
@@ -9,17 +9,17 @@ class Connection:
 
     def __new__(
         cls,
-        host,
-        port,
-        username,
-        password,
-        db_name,
-        context=None,
-        header=False,
-        ssl_verify_cert=False,
-        ssl_client_cert=None,
-        proxies=None,
-    ) -> None:
+        host: str,
+        port: str,
+        username: str,
+        password: str,
+        db_name: str,
+        context: str = None,
+        header: bool = False,
+        ssl_verify_cert: bool = False,
+        ssl_client_cert: str = None,
+        proxies: str = None,
+    ) -> "Connection":
         return connect(
             engine_url=host, database=db_name, username=username, password=password
         )
